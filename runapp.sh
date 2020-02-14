59cd219124e0aa555f9adc5cb03450eeda7fa9c2
Dilip Kunderu <venkata.sai.rama.dilip.kunder@oracle.com>
2020-02-13 18:49:17 -0800

    nohup again 2

--- runapp.sh
+++ runapp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot App'
 cd '/home/ubuntu/set-impl'
-nohup java -jar abstractset-0.0.1-SNAPSHOT.war >/dev/null 2>&1
+nohup java -jar abstractset-0.0.1-SNAPSHOT.war >/dev/null 2>&1 &
